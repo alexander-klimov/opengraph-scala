16214921e15a15e6279bfa7afdeec23cb1a51db0
Francis Carr <fran@fcarr.com>
2015-09-09 22:49:11 +0100

    Add OpenGraphKeys

---
+++ src/main/scala/model/OpenGraphKeys.scala
@@ -0,0 +1,9 @@
+package model
+
+object OpenGraphKeys {
+
+  val ogTitle: String = "og:title"
+  val ogType: String = "og:type"
+  val ogUrl: String = "og:url"
+  val ogImage: String = "og:image"
+}
