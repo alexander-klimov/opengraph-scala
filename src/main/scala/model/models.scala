493c23f990b5be5d2e72cdeebf43dfc139256c7f
Francis Carr <fran@fcarr.com>
2015-09-12 23:11:31 +0100

    Add MetaData case class

--- src/main/scala/model/models.scala
+++ src/main/scala/model/models.scala
@@ -9,6 +9,16 @@ case class Basic(
   url: String
 ) extends OpenGraphElement
 
+case class MetaData(
+  audio: String,
+  description: String,
+  determiner: String,
+  locale: String,
+  localeAlternative: List[String],
+  siteName: String,
+  video: String
+)
+
 case class OpenGraphRequest(url: String)
 
 
