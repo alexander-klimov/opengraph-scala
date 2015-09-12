a4697ee427f7d0f39b0992b300456a070cd5419d
Francis Carr <fran@fcarr.com>
2015-09-12 23:05:00 +0100

    Add opengraph optional metadata keys

--- src/main/scala/model/OpenGraphKeys.scala
+++ src/main/scala/model/OpenGraphKeys.scala
@@ -2,8 +2,18 @@ package model
 
 object OpenGraphKeys {
 
+  //Basic Metadata Keys
   val ogTitle: String = "og:title"
   val ogType: String = "og:type"
   val ogUrl: String = "og:url"
   val ogImage: String = "og:image"
+
+  //Optional Metadata Keys
+  val ogAudio: String = "og:audio"
+  val ogDescription: String = "og:description"
+  val ogDeterminer: String = "og:determiner"
+  val ogLocale: String = "og:locale"
+  val ogLocaleAlternative: String = "og:locale:alternate"
+  val ogSiteName: String = "og:site_name"
+  val ogVideo: String = "og:video"
 }
